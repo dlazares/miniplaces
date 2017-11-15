e2ab54801f1bda1c9645021fcb24399d70cfdfb9
Diego Lazares <diego.lazares@gmail.com>
2017-11-15 14:18:50 -0500

    modified image loading and added test parameters

--- model/tensorflow/resnet_test.py
+++ model/tensorflow/resnet_test.py
@@ -13,7 +13,7 @@ fine_size = 224
 c = 3
 data_mean = np.asarray([0.45834960097,0.44674252445,0.41352266842])
 
-# Training Parameters
+# Test Parameters
 learning_rate = 0.0001
 dropout = 0.5 # Dropout, probability to keep units
 training_iters = 50000
@@ -21,26 +21,7 @@ step_display = 50
 step_save = 500
 path_save = './checkpoints/resnet_finetune'
 start_from = './checkpoints/resnet-1000'
-
-# Construct dataloader
-opt_data_train = {
-    #'data_h5': 'miniplaces_256_train.h5',
-    'data_root': '../../data/images/',   # MODIFY PATH ACCORDINGLY
-    'data_list': '../../data/train.txt', # MODIFY PATH ACCORDINGLY
-    'load_size': load_size,
-    'fine_size': fine_size,
-    'data_mean': data_mean,
-    'randomize': True
-    }
-opt_data_val = {
-    #'data_h5': 'miniplaces_256_val.h5',
-    'data_root': '../../data/images/',   # MODIFY PATH ACCORDINGLY
-    'data_list': '../../data/val.txt',   # MODIFY PATH ACCORDINGLY
-    'load_size': load_size,
-    'fine_size': fine_size,
-    'data_mean': data_mean,
-    'randomize': False
-    }
+test_dir = "../../data/images/test"
 
 opt_data_test = {
     #'data_h5': 'miniplaces_256_val.h5',
@@ -51,12 +32,6 @@ opt_data_test = {
     'data_mean': data_mean,
     'randomize': False
     }
-
-
-loader_train = DataLoaderDisk(**opt_data_train)
-loader_val = DataLoaderDisk(**opt_data_val)
-#loader_train = DataLoaderH5(**opt_data_train)
-#loader_val = DataLoaderH5(**opt_data_val)
 
 # tf Graph input
 x = tf.placeholder(tf.float32, [None, fine_size, fine_size, c])
@@ -96,11 +71,23 @@ with tf.Session() as sess:
             for j in range(group_size * i,min(len(files),group_size*(i+1))):
                 filepath = os.path.join(test_dir, files[j])
                 image = scipy.misc.imread(filepath)
-                image = scipy.misc.imresize(image, (size[0], size[1],3))
+                image = scipy.misc.imresize(image, (load_size, load_size))
+                image = image.astype(np.float32)/255.
+                image = image - np.array(data_mean)
+            
+                offset_h = (load_size-fine_size)/2
+                offset_w = (load_size-fine_size)/2
+
+                offset_h = int(offset_h)
+                offset_w = int(offset_w)    
+            
+                image =  image[offset_h:offset_h+self.fine_size, offset_w:offset_w+self.fine_size, :]
+
                 test_im.append(image)
                 filenames.append(files[j])
+                
             test_im = np.array(test_im)
-            test_im = test_im.reshape(-1, 100, 100, 3).astype('float32') / 255.
+            
             print("predicting on ",test_im.shape[0]," images")
             out = tf.nn.top_k(tf.softmax(logits),k=5,sorted=True)
             top_values,top_indices = sess.run([out], feed_dict={x: test_im, y: labels_batch, keep_dropout: 1., train_phase: False}) 
