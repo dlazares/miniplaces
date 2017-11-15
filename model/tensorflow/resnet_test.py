a99967e943feb4f7581f28c1e236b55475a21c6f
Ubuntu <ubuntu@ip-172-31-47-240.ec2.internal>
2017-11-15 19:41:08 +0000

    resnet_test working. added results file

--- model/tensorflow/resnet_test.py
+++ model/tensorflow/resnet_test.py
@@ -20,8 +20,9 @@ training_iters = 50000
 step_display = 50
 step_save = 500
 path_save = './checkpoints/resnet_finetune'
-start_from = './checkpoints/resnet-1000'
+start_from = './checkpoints/resnet_finetunev2-4000'
 test_dir = "../../data/images/test"
+groups=100
 
 opt_data_test = {
     #'data_h5': 'miniplaces_256_val.h5',
@@ -62,9 +63,9 @@ with tf.Session() as sess:
         sess.run(init)
     
     files = os.listdir(test_dir)
-    group_size = int(len(files) / args.groups)
+    group_size = int(len(files) / groups)
     with open("results.txt","w") as result_file:
-        for i in range(args.groups):
+        for i in range(groups):
             print("Running on group",i)
             test_im = []
             filenames = []
@@ -81,7 +82,7 @@ with tf.Session() as sess:
                 offset_h = int(offset_h)
                 offset_w = int(offset_w)    
             
-                image =  image[offset_h:offset_h+self.fine_size, offset_w:offset_w+self.fine_size, :]
+                image =  image[offset_h:offset_h+fine_size, offset_w:offset_w+fine_size, :]
 
                 test_im.append(image)
                 filenames.append(files[j])
@@ -89,10 +90,10 @@ with tf.Session() as sess:
             test_im = np.array(test_im)
             
             print("predicting on ",test_im.shape[0]," images")
-            out = tf.nn.top_k(tf.softmax(logits),k=5,sorted=True)
-            top_values,top_indices = sess.run([out], feed_dict={x: test_im, y: labels_batch, keep_dropout: 1., train_phase: False}) 
+            out_vals,out_indices = tf.nn.top_k(tf.nn.softmax(logits),k=5,sorted=True)
+            top_values,top_indices = sess.run([out_vals,out_indices], feed_dict={x: test_im,  keep_dropout: 1., train_phase: False}) 
 
-            print("writing to file")
+            print("writing to file",filenames[0],top_indices[0],top_values[0])
             for l in range(len(filenames)):
                 fn = filenames[l]
                 vals = " ".join(map(str,top_indices[l]))
